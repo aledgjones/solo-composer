32c25f2ff2fac8dd40fe5fa5eebc2da68bb1eeb0
Aled Jones <aledjones.viola@gmail.com>
2020-05-27 16:40:58 +0100

    parse

--- src/parse/use-parse.ts
+++ src/parse/use-parse.ts
@@ -1,11 +1,10 @@
-import { useMemo, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { FlowKey } from "../services/score-flow";
 import { RenderInstructions } from "../render/instructions";
 import { Score } from "../services/score";
 import { useMM } from "./converter";
 
-import myWorker from "./parse.worker";
 import wasm from "solo-composer-parser/solo_composer_parser_bg.wasm";
 import init, { InitOutput } from "solo-composer-parser";
 
@@ -23,6 +22,41 @@ export function useParseWorker(score: Score, flowKey: FlowKey) {
     const mm = useMM();
 
     useEffect(() => {
+        import { useMemo, useEffect, useState } from "react";
+
+        import { FlowKey } from "../services/score-flow";
+        import { RenderInstructions } from "../render/instructions";
+        import { Score } from "../services/score";
+        import { useMM } from "./converter";
+
+        import myWorker from "./parse.worker";
+
+        export function useParseWorker(score: Score, flowKey: FlowKey) {
+            const [instructions, setInstructions] = useState<RenderInstructions>();
+
+            const worker = useMemo(() => {
+                return new myWorker() as Worker;
+            }, []);
+
+            const mm = useMM();
+
+            useEffect(() => {
+                worker.postMessage({ mm, score, flowKey });
+            }, [mm, score, flowKey, worker]);
+
+            useEffect(() => {
+                const cb = (e: any) => {
+                    setInstructions(e.data);
+                };
+                worker.addEventListener("message", cb);
+                return () => {
+                    worker.removeEventListener("message", cb);
+                    worker.terminate();
+                };
+            }, [worker]);
+
+            return instructions;
+        }
         parser?.greet();
     }, [parser]);
 
